263400ee74961575cfe54a6dd2c877c6d22bd680
AlanGarcia1 <101156029+AlanGarcia1@users.noreply.github.com>
2025-10-20 17:43:06 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// JavaScript Document// Ejemplo: validación simple del formulario
+document.getElementById('contactForm').addEventListener('submit', function(e){
+    e.preventDefault();
+    alert('Mensaje enviado (simulado)');
+});
